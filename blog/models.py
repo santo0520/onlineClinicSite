42cc4f6866ce0337a67a685a1be5b1cf2b38329f
Sanders Lin <coolsanderslin@gmail.com>
2023-11-28 13:47:10 +0800

    initial commit. Basic blog page and blog indexx page uimplented. need to add menus and design for functioning MVP

---
+++ blog/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+
+# Add these:
+from modelcluster.fields import ParentalKey
+from wagtail.models import Page, Orderable
+from wagtail.fields import RichTextField, StreamField
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.search import index
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = StreamField([
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('richTextParagraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('quote', blocks.BlockQuoteBlock()),
+        ('paragraph', blocks.TextBlock()), 
+        ('rawHTML',blocks.RawHTMLBlock(help_text="for ordered list, use class='list-numbers' 2.")),
+        ('twoImage', blocks.StructBlock([
+        	('image1',ImageChooserBlock(required=True)),
+        	('image2', ImageChooserBlock(required=True)), 
+
+        	])), #need caption, also one image (wide and standard )
+        ('highlight', blocks.TextBlock()), #not implemented yet
+
+    ], use_json_field=True)
+
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None	
+
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+class BlogPageGalleryImage(Orderable):
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('caption'),
+    ]
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5uyj-a!%+n)o!7p3tjwh_zp(p@)g7_bg=-&im8(%+3vc5u4+3^"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
